0c564c645f84c7b0ebbb107e143b439a6e3ea43d
et41 <erenturan41@gmail.com>
2018-10-22 17:03:50 +0300

    fix: checkbox style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 var map;
-
+let devices = ["computer", "television", "refrigerator"];
 function initMap() {
 map = new google.maps.Map(document.getElementById('mapbox'), {
   center: {lat: -34.397, lng: 150.644},
   zoom: 10
 });
 }
+
+let el = document.getElementById("selectbox");
+
+el.addEventListener('click', (event) => {
+	console.log('click:', event.target);
+	let deviceId =  event.target.id;
+	var reg = /\d+/g;
+		let deviceNumber = Number(deviceId.match(reg));
+
+	if(deviceNumber != 0 ){
+		console.log('clicked:', devices[deviceNumber - 1]);
+	}
+});
